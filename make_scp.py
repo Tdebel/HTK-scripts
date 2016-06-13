022399e11713c9fc2414e30dcec8cbb07b14a92e
Marein Konings <Marein.Koening@mpi.nl>
2016-06-13 13:39:57 +0200

    Make make_scp loadable as a python module

--- make_scp.py
+++ make_scp.py
@@ -1,22 +1,23 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Create a \'script file\' containing sound file paths and their corresponding .mfc file paths.')
-parser.add_argument('indir', help='The directory to search for files. All files will be listed.')
-parser.add_argument('outdir', help='The directory that .mfc paths will lead to.')
-parser.add_argument('outfile', help='The path to the output file.')
-args = parser.parse_args()
+def make_scp(indir, outdir, outfile):
 
-def inpath(filename):
-    return os.path.join(args.indir, filename)
+    def inpath(filename):
+        return os.path.join(indir, filename)
 
-def outpath(filename):
-    name, _ = os.path.splitext(filename)
-    return os.path.join(args.outdir, name + '.mfc')
+    def outpath(filename):
+        name, _ = os.path.splitext(filename)
+        return os.path.join(outdir, name + '.mfc')
 
-scp = {inpath(f):outpath(f) for f in os.listdir(args.indir)}
+    with open(outfile,'w') as outfile:
+        for f in os.listdir(indir):
+            outfile.write('{} {}\n'.format(inpath(f),outpath(f)))
 
-with open(args.outfile,'w') as f:
-    for i,o in scp.items():
-        f.write('{} {}\n'.format(i,o))
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a \'script file\' containing sound file paths and their corresponding .mfc file paths.')
+    parser.add_argument('indir', help='The directory to search for files. All files will be listed.')
+    parser.add_argument('outdir', help='The directory that .mfc paths will lead to.')
+    parser.add_argument('outfile', help='The path to the output file.')
+    args = parser.parse_args()
+    make_scp(args.indir, args.outdir, args.outfile)
