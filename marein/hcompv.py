77a54c62b9b4fbe2662297c23cc6b228c209dfc8
Marein Konings <Marein.Koening@mpi.nl>
2016-06-20 15:28:14 +0200

    add unfinished version of hcompv.py

---
+++ marein/hcompv.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+import subprocess
+
+# write a hcopy config file to filename based on args (same as in hcopy.py, should be outsourced)
+def make_config(args,filename):
+    with open(filename,'w') as f:
+        t = most_common_type(args.indir)
+        if t.lower() not in SUPPORTED_TYPES:
+            raise ValueError('Filetype {} is not supported by HTK.'.format(t))
+        f.write('SOURCEFORMAT = {}\n'.format(t.upper()))
+        f.write('TARGETKIND = {}\n'.format(args.targetkind))
+        f.write('TARGETRATE = {}\n'.format(args.targetrate))
+        f.write('SAVECOMPRESSED = {}\n'.format(args.compressed))
+
+def hcompv(args):
+    config_file = 'hcompv_temp.config'
+    make_config(args,config_file)
+    subprocess.call('HCompV', '-C', config_file, '-f', args.floor, '-m', '-S', args.script, '-H', args.outdir, args.proto)
+    os.remove(config_file)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('proto', help='The initial prototype file.')
+    parser.add_argument('script', help='Script file (.scp).')
+    parser.add_argument('outdir', help='Dir to write HMM macro files.')
+    parser.add_argument('-k', '--targetkind', nargs='?', default='MFCC_0_D', type=str)
+    parser.add_argument('-r', '--targetrate', nargs='?', default=100000.0, type=float)
+    parser.add_argument('-c', '--compressed', nargs='?', default='T', choices=['T','F'])
+    parser.add_argument('-f', '--floor', nargs='?', default=0.01, type=float)
+    parser.add_argument('-m', '--means', default=True, action='store_true')
+    args = parser.parse_args()
+    hcompv(args)
