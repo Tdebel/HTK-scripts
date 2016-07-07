23e7db113665db36de343d40b05c1d607e8941e6
Marein Konings <Marein.Koening@mpi.nl>
2016-07-07 14:48:37 +0200

    make prepare command-line capable

--- marein/prepare.py
+++ marein/prepare.py
@@ -1,4 +1,5 @@
 import os, re
+import argparse
 import functools
 import subprocess
 from collections import Counter
@@ -49,19 +50,29 @@ def hcopy(indir,outdir,config_file):
         os.mkdir(outdir)
     subprocess.call(['HCopy','-C',config_file,'-S',scp_file])
 		
-def prepare(in_dir,out_dir,regex,config_file,word_file,dict_file):
-	gram_folder('gram',in_dir,regex)
+def prepare(indir,outdir,regex,config,dict):
+	gram_folder('gram',indir,regex)
 	call('HParse gram wdnet')
-	call('cat {} | LC_ALL=C sort | uniq > wlist'.format(word_file))
-	call('cat {} | LC_ALL=C sort | uniq > ordered_dict'.format(dict_file))
+	call("cut -d' ' -f1 {} > words".format(dict))
+	call('cat words | LC_ALL=C sort | uniq > wlist')
+	call('cat {} | LC_ALL=C sort | uniq > ordered_dict'.format(dict))
 	replace_numbers('ordered_dict')
 	call("sed -e 's/\t/\t1\t/' -i ordered_dict")
 	call('HDMan -m -w wlist -n monophones -i dict ordered_dict')
 	call("echo 'sil' >> monophones")
 	call("echo -e 'silence\t[silence]\tsil' >> dict")
 	call("sort dict -o dict")
-	mlf_folder('words.mlf',in_dir,regex)
-	hcopy(in_dir,out_dir,config_file)
+	mlf_folder('words.mlf',indir,regex)
+	hcopy(indir,outdir,config)
 	
-prepare('../../material/segmentations','mfc','cut_\d*_\d*_(\w*).wav','input/config','input/words.txt','input/current_wordlist.txt')
-	
+# python prepare.py ../../material/segmentations mfc "cut_\d*_\d*_(\w*).wav" input/config input/current_wordlist.txt
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('indir', help='The directory of the input sound files.')
+    parser.add_argument('outdir', help='The directory to create the prepared files in.')
+    parser.add_argument('regex', help='The regex for extracting words from the sound file names.')
+    parser.add_argument('config', help='The config file path.')
+    parser.add_argument('dict', help='The word->phoneme dictionary file path')
+    args = parser.parse_args()
+    prepare(args.indir,args.outdir,args.regex,args.config,args.dict)
