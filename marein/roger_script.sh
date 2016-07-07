15f45bc0cb3ef416e092150bbe6ae677269ffefd
Marein Konings <Marein.Koening@mpi.nl>
2016-07-07 12:08:32 +0200

    add herest script and replace_numbers script

--- marein/roger_script.sh
+++ marein/roger_script.sh
@@ -5,10 +5,14 @@ python gram.py gram --folder $INFILES --regex "cut_\d*_\d*_(\w*).wav"
 HParse gram wdnet
 cat $WORDS | LC_ALL=C sort | uniq > wlist
 cat $DICT | LC_ALL=C sort | uniq | sed -e 's/\t/\t1\t/' > ordered_dict
+python replace_numbers.py ordered_dict
 HDMan -m -w wlist -n monophones -i dict ordered_dict
+echo 'sil' >> monophones
 python mlf.py mlf --folder $INFILES --regex "cut_\d*_\d*_(\w*).wav"
 python hcopy.py $INFILES mfc
 python proto.py proto
 cat hcopy_temp.scp | cut -d " " -f 2 > hcompv_temp.scp
 python hcompv.py proto hcompv_temp.scp hmmdef0
 python hled.py phones.mlf ordered_dict mlf
+python hmmcopy.py
+python herest.py phones.mlf hcompv_temp.scp hmm0 hmms monophones
