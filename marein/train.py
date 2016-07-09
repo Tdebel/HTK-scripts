83eca77bc2ce8052c55fc7ce8d3cf1750f2e3bb0
Marein Konings <Marein.Koening@mpi.nl>
2016-07-09 11:55:18 +0200

    finish train.py

--- marein/train.py
+++ marein/train.py
@@ -16,7 +16,6 @@ def hled(outfile,dictfile,wordsfile,s=False):
     subprocess.call(['HLEd', '-l', '*', '-d', dictfile, '-i', outfile, ledfile, wordsfile])
     os.remove(ledfile)
 
-# write a hcopy config file to filename based on args
 def macro(protodir,outdir):
     vfloors = open(os.path.join(protodir,'vFloors'))
     proto = open(os.path.join(protodir,'proto'))
@@ -26,24 +25,52 @@ def macro(protodir,outdir):
     macros.write("\n" + vfloors.read())
 
 def hmmdefs(protodir,monophones,outdir):
-    monophones = open(os.path.join(protodir,'proto')
-    proto = open('monophones')
-    hmmdefs = open(os.path.join(outdir,'hmmdefs', 'w')
+    monophones = open(monophones)
+    proto = open(os.path.join(protodir,'proto'))
+    hmmdefs = open(os.path.join(outdir,'hmmdefs'), 'w')
     
     mono_list = monophones.read().splitlines()
     proto_temp = proto.read().splitlines()[4:]
     
     for phone in mono_list:
-        hmmdefs.write("~h \"" + phone + "\"\n")
-	hmmdefs.write("\n".join(proto_temp) + "\n")
-    
-def train(config,scp,outdir,proto,dict,mlf,floor=0.01):
-    if not os.exists(outdir):
+        hmmdefs.write('~h "' + phone + '"\n')
+        hmmdefs.write('\n'.join(proto_temp) + '\n')
+
+def herest(config,mlf,script,macrodir,hmms,outdir,pruning=[250,150,1000],n=6):
+    t = ['{:.1f}'.format(p) for p in pruning]
+    cur_dir = macrodir
+    macros = ['macros','hmmdefs']
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    for i in range(n):
+        H = [item for pair in zip(['-H']*len(macros),[os.path.join(cur_dir,macro) for macro in macros]) for item in pair]
+        cur_dir = os.path.join(outdir,outdir+str(i))
+        if not os.path.exists(cur_dir):
+            os.mkdir(cur_dir)
+        subprocess.call(['HERest', '-C', config, '-I', mlf, '-t'] + t + ['-S', script] + H + ['-M', cur_dir, hmms])
+
+def train(config,scp,outdir,proto,dict,mlf,monophones,floor=0.01):
+    if not os.path.exists(outdir):
         os.mkdir(outdir)
     call('cat {} | cut -d " " -f 2 > scp2'.format(scp))
     call('HCompV -C {} -f {} -m -S scp2 -M {} {}'.format(config,floor,outdir,proto))
     hled('phones.mlf',dict,mlf)
-    macro(outdir,outdir+2) #???
-    
+    if not os.path.exists(outdir+'2'):
+        os.mkdir(outdir+'2')
+    macro(outdir,outdir+'2') #???
+    hmmdefs(outdir,monophones,outdir+'2')
+    herest(config,'phones.mlf','scp2',outdir+'2',monophones,outdir)
 	
-train('input/config','scp','htk','input/proto','ordered_dict','mlf')
+# python train.py htk scp ordered_dict input/proto words.mlf monophones input/config
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('outdir', help='The working directory for this script (will contain hmmdef# folders).')
+    parser.add_argument('scp', help='The script file (mfc file list).')
+    parser.add_argument('dict', help='The word->phoneme dictionary file path')
+    parser.add_argument('proto', help='The input proto file')
+    parser.add_argument('mlf', help='The input mlf file.')
+    parser.add_argument('monophones', help='The input monophones file.')
+    parser.add_argument('config', help='The config file path.')
+    args = parser.parse_args()
+    train(args.config, args.scp, args.outdir, args.proto, args.dict, args.mlf, args.monophones)
